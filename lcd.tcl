a4f7f409bdf2c2020cc4e28504ecda9c73239e71
tbh111 <49756698+tbh111@users.noreply.github.com>
2020-09-29 19:47:06 +0800

    Add files via upload

---
+++ lcd.tcl
@@ -0,0 +1,33 @@
+# Copyright (C) 1991-2013 Altera Corporation
+# Your use of Altera Corporation's design tools, logic functions 
+# and other software and tools, and its AMPP partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Altera Program License 
+# Subscription Agreement, Altera MegaCore Function License 
+# Agreement, or other applicable license agreement, including, 
+# without limitation, that your use is for the sole purpose of 
+# programming logic devices manufactured by Altera and sold by 
+# Altera or its authorized distributors.  Please refer to the 
+# applicable agreement for further details.
+
+# Quartus II 32-bit Version 13.0.0 Build 156 04/24/2013 SJ Full Version
+# File: E:\lock\lcd_test\lcd.tcl
+# Generated on: Sat Sep 26 15:55:51 2020
+
+package require ::quartus::project
+
+set_location_assignment PIN_88 -to clk
+set_location_assignment PIN_120 -to lcd_rs
+set_location_assignment PIN_24 -to rst_n
+set_location_assignment PIN_49 -to lcd_data[7]
+set_location_assignment PIN_50 -to lcd_data[6]
+set_location_assignment PIN_51 -to lcd_data[5]
+set_location_assignment PIN_52 -to lcd_data[4]
+set_location_assignment PIN_53 -to lcd_data[3]
+set_location_assignment PIN_54 -to lcd_data[2]
+set_location_assignment PIN_55 -to lcd_data[1]
+set_location_assignment PIN_58 -to lcd_data[0]
+set_location_assignment PIN_121 -to lcd_en
+set_location_assignment PIN_125 -to lcd_rw
